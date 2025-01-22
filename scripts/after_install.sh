a3ac38381ad1d8f1e4fe82800fc40ceac4602a6c
Ramon Ribeiro <ramon.ribeiro@cadastra.com>
2025-01-22 05:34:41 -0300

    add cd

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo 'yarn install && yarn build' >> /home/ramon/todos/deploy.log 
+cd /home/ramon/todos
 yarn install && yarn build >> /home/ramon/todos/deploy.log
 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo 'run application_start.sh: ' >> /home/ramon/todos/deploy.log
-
-echo 'yarn start' >> /home/ramon/todos/deploy.log
+cd /home/ramon/todos
 yarn restart >> /home/ramon/todos/deploy.log
