cf80f572e6757a76e79f3d3f2afef0b1a8bef5d9
Ramon Ribeiro <ramon.ribeiro@cadastra.com>
2025-01-22 03:47:09 -0300

    change path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'yarn install' >> /home/ramon/nodejs-aws-codedeploy-pipeline/deploy.log 
-yarn >> /home/ramon/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'yarn install' >> /home/ramon/dev/todo/deploy.log 
+yarn >> /home/ramon/dev/todo/deploy.log
 
-echo 'yarn build' >> /home/ramon/nodejs-aws-codedeploy-pipeline/deploy.log 
-yarn build >> /home/ramon/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'yarn build' >> /home/ramon/dev/todo/deploy.log 
+yarn build >> /home/ramon/dev/todo/deploy.log
